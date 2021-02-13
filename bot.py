1542bc0c0d28b4d6c493bbfbb7a3b1a1a1c48800
Ranunculus <tatianamalyutina@gmail.com>
2021-02-14 00:08:48 +0300

    bot for Telegram: WIP user sessions

--- bot.py
+++ bot.py
@@ -1,7 +1,6 @@
 import telebot
 from fuzzywuzzy import fuzz as fuzzy_search
 from telebot import types
-
 from portugués_trainer import database_operations as database
 
 
@@ -11,39 +10,61 @@ def token():
 
 
 bot = telebot.TeleBot(token())
-
-answers = {}
+database.init_database()
 
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
     keyboard = types.InlineKeyboardMarkup()
-    train_option = types.InlineKeyboardButton(text='Учить слова', callback_data='train')
-    keyboard.add(train_option)
-    add_new_option = types.InlineKeyboardButton(text='Добавим новые слова', callback_data='new')
-    keyboard.add(add_new_option)
-    bot.send_message(message.from_user.id, text="Что будем делать?", reply_markup=keyboard)
+    user_id = message.from_user.id
+    database.create_user_if_doesnt_exist(user_id, message.from_user.first_name)
+    if database.has_words_to_train(user_id):
+        bot_train_option(keyboard)
+    else:
+        add_existing_prompt = types.InlineKeyboardButton(text='Скопировать текущую версию словаря для тренировок', callback_data='add_existing')
+        keyboard.add(add_existing_prompt)
+    bot_add_new_word(keyboard)
+    bot.send_message(user_id, text=f"{message.from_user.first_name}, что будем делать?", reply_markup=keyboard)
+
+
+def bot_add_new_word(keyboard):
+    keyboard.add(types.InlineKeyboardButton(text='Добавим новые слова вручную', callback_data='new'))
+
+
+def bot_train_option(keyboard):
+    keyboard.add(types.InlineKeyboardButton(text='Учить слова', callback_data='train'))
+
+
+def add_existing_words_for_user_training(user_id):
+    database.create_trainings_of_all_words_for_user(user_id)
 
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_worker(call):
     if call.data == "train":
-        train_next_word_prompt(call.message.chat)
+        train_next_word_prompt(call.message.chat, call.from_user.id)
     elif call.data == "new":
         add_new_word_prompt(call.message.chat)
+    elif call.data == "add_existing":
+        user_id = call.from_user.id
+        add_existing_words_for_user_training(user_id)
+        keyboard = types.InlineKeyboardMarkup()
+        bot_train_option(keyboard)
+        bot_add_new_word(keyboard)
+        bot.send_message(user_id, text="Готово! Что дальше?", reply_markup=keyboard)
 
 
 def add_new_word_prompt(chat):
     bot.send_message(chat.id, "Введи через запятую:слово,произношение,перевод")
 
 
-def train_next_word_prompt(chat):
-    words_to_train = database.next_word_for_training()
+def train_next_word_prompt(chat, user_id):
+    words_to_train = database.next_word_for_training(user_id)
     if len(words_to_train) == 0:
         bot.send_message(chat.id, "Молодец! Пока все слова повторены")
     else:
         next_word_for_training = words_to_train[0]
-        answers[chat.username] = (next_word_for_training[0], next_word_for_training[1])
+        database.save_session_for_user(user_id, next_word_for_training[0])
         bot.send_message(chat.id, next_word_for_training[3])
 
 
@@ -54,17 +75,18 @@ def handle_word(message):
         database.create_new_word(message_split[0], message_split[1], message_split[2])
         add_new_word_prompt(message.chat)
     else:
-        answer_tuple = answers[message.chat.username]
-        answer = answer_tuple[1]
-        ratio = fuzzy_search.ratio(answer, message.text)
+        current_session = database.get_session_for_user(message.from_user.id)
+        answer = database.get_word(current_session[2])
+        ratio = fuzzy_search.ratio(answer[1], message.text)
         if ratio == 100:
             bot.send_message(message.chat.id, "Отлично!")
         else:
             # todo: hints?
             bot.send_message(message.chat.id, f"Правильный ответ: {answer}")
 
-        database.save_training_result(answer_tuple[0], ratio)
-        train_next_word_prompt(message.chat)
+        database.save_training_result(message.from_user.id, answer[0], ratio)
+        database.delete_session_for_user(message.from_user.id)
+        train_next_word_prompt(message.chat, message.from_user.id)
 
 
 bot.polling()
--- database_operations.py
+++ database_operations.py
@@ -1,5 +1,5 @@
-import sqlite3
 import datetime
+import sqlite3
 
 
 def get_connection():
@@ -9,25 +9,45 @@ def get_connection():
 def init_database():
     conn = get_connection()
     cur = conn.cursor()
+
     cur.execute('''CREATE TABLE IF NOT EXISTS Words
         (id INTEGER PRIMARY KEY, word TEXT, transcription TEXT, translation TEXT)''')
 
+    cur.execute('''CREATE TABLE IF NOT EXISTS Users
+        (id INTEGER PRIMARY KEY, 
+        first_name TEXT)''')
+
     cur.execute('''CREATE TABLE IF NOT EXISTS Trainings
-        (id INTEGER PRIMARY KEY, word_id INTEGER, successes INTEGER, failures INTEGER, next_training_date INTEGER,
-        FOREIGN KEY(word_id) REFERENCES Words(id))''')
+        (id INTEGER PRIMARY KEY, 
+        word_id INTEGER, 
+        user_id INTEGER,
+        successes INTEGER, 
+        failures INTEGER, 
+        next_training_date INTEGER,
+        FOREIGN KEY(word_id) REFERENCES Words(id),
+        FOREIGN KEY(user_id) REFERENCES Users(id),
+        UNIQUE(word_id, user_id))''')
+
+    cur.execute('''CREATE TABLE IF NOT EXISTS Sessions
+        (id INTEGER PRIMARY KEY, 
+        user_id INTEGER,
+        word_id INTEGER,
+        FOREIGN KEY(word_id) REFERENCES Words(id),
+        FOREIGN KEY(user_id) REFERENCES Users(id))''')
+
     cur.close()
     conn.close()
 
 
-def create_new_word(word, transcription, translation):
+def create_new_word(user_id, word, transcription, translation):
     connection = get_connection()
     cursor = connection.cursor()
     cursor.execute('INSERT OR IGNORE INTO Words (word, transcription, translation) VALUES ( ?, ?, ? )',
                    (word, transcription, translation))
 
     word_id = cursor.execute('SELECT id FROM Words WHERE word = (?)', (word,)).fetchall()
-    cursor.execute('INSERT OR IGNORE INTO Trainings (word_id, successes, failures) VALUES ( ?, ?, ? )',
-                   (word_id[0][0], 0, 0))
+    cursor.execute('INSERT OR IGNORE INTO Trainings (user_id, word_id, successes, failures) VALUES ( ?, ?, ?, ? )',
+                   (user_id, word_id[0][0], 0, 0))
     connection.commit()
     cursor.close()
     connection.close()
@@ -45,11 +65,11 @@ def list_all():
     return rows
 
 
-def list_words_for_training(words_amount):
+def list_words_for_training(user_id, words_amount):
     connection = get_connection()
     cursor = connection.cursor()
     cursor.execute(
-        f"SELECT word_id FROM Trainings WHERE next_training_date IS NULL OR next_training_date < {datetime.datetime.now().timestamp()} LIMIT {words_amount}")
+        f"SELECT word_id FROM Trainings WHERE (next_training_date IS NULL OR next_training_date < {datetime.datetime.now().timestamp()}) AND user_id = {user_id} LIMIT {words_amount}")
 
     word_ids = cursor.fetchall()
 
@@ -58,7 +78,7 @@ def list_words_for_training(words_amount):
         result = []
         for t in word_ids:
             result.append(t[0])
-        join = ','.join(['?']*len(result))
+        join = ','.join(['?'] * len(result))
         query = f"SELECT * FROM Words WHERE id IN ({join})"
         rows = cursor.execute(query, result).fetchall()
 
@@ -67,22 +87,90 @@ def list_words_for_training(words_amount):
     return rows
 
 
-def next_word_for_training():
-    return list_words_for_training(1)
+def next_word_for_training(user_id):
+    return list_words_for_training(user_id, 1)
 
 
-def save_training_result(word_id, ratio):
+def save_training_result(user_id, word_id, ratio):
     connection = get_connection()
     cursor = connection.cursor()
     if ratio == 100:
-        cursor.execute('UPDATE Trainings SET successes = successes + 1, next_training_date = ? WHERE word_id = (?)', ((datetime.datetime.now() + datetime.timedelta(days=1)).timestamp(), word_id,))
+        cursor.execute('UPDATE Trainings SET successes = successes + 1, next_training_date = ? WHERE word_id = ? and user_id = ?',
+                       ((datetime.datetime.now() + datetime.timedelta(days=1)).timestamp(), word_id, user_id, ))
     elif ratio > 85:
-        cursor.execute('UPDATE Trainings SET failures = failures + 1, next_training_date = ? WHERE word_id = ?',
-                       ((datetime.datetime.now() + datetime.timedelta(minutes=5)).timestamp(), word_id))
+        cursor.execute('UPDATE Trainings SET failures = failures + 1, next_training_date = ? WHERE word_id = ? and user_id = ?',
+                       ((datetime.datetime.now() + datetime.timedelta(minutes=5)).timestamp(), word_id, user_id, ))
     else:
-        cursor.execute('UPDATE Trainings SET failures = failures + 1, next_training_date = ? WHERE word_id = ?',
-                       ((datetime.datetime.now() + datetime.timedelta(minutes=1)).timestamp(), word_id))
+        cursor.execute('UPDATE Trainings SET failures = failures + 1, next_training_date = ? WHERE word_id = ? and user_id = ?',
+                       ((datetime.datetime.now() + datetime.timedelta(minutes=1)).timestamp(), word_id, user_id, ))
 
     connection.commit()
     cursor.close()
     connection.close()
+
+
+def create_user_if_doesnt_exist(telegram_user_id, telegram_user_first_name):
+    write_data('INSERT OR IGNORE INTO Users (id, first_name) VALUES ( ?, ? )', (telegram_user_id, telegram_user_first_name))
+
+
+def has_words_to_train(telegram_user_id):
+    connection = get_connection()
+    cursor = connection.cursor()
+    trainings = cursor.execute(
+        f"SELECT * FROM Trainings t JOIN Users u on t.user_id = u.id WHERE u.id = {telegram_user_id} LIMIT 1").fetchall()
+    cursor.close()
+    connection.close()
+    return len(trainings) > 0
+
+
+def create_trainings_of_all_words_for_user(user_id):
+    connection = get_connection()
+    cursor = connection.cursor()
+    words = cursor.execute(f"SELECT * FROM Words").fetchall()
+
+    for word in words:
+        try:
+            cursor.execute('INSERT OR IGNORE INTO Trainings (user_id, word_id, successes, failures) VALUES ( ?, ?, ?, ? )',
+                           (user_id, word[0], 0, 0))
+        except:
+            print(f"Trying to insert duplicate of {word[0]} for user {user_id}")
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+
+def save_session_for_user(user_id, word_id):
+    write_data('INSERT OR IGNORE INTO Sessions (user_id, word_id) VALUES ( ?, ? )', (user_id, word_id))
+
+
+def write_data(query, data):
+    connection = get_connection()
+    cursor = connection.cursor()
+    cursor.execute(query, data)
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+
+def get_session_for_user(user_id):
+    connection = get_connection()
+    cursor = connection.cursor()
+    session = cursor.execute(f"SELECT * FROM Sessions WHERE user_id = {user_id}").fetchall()
+    cursor.close()
+    connection.close()
+    #todo return session[0] if len(session) == 1 else raise Exception('I know Python!')
+    return session[0]
+
+
+def get_word(word_id):
+    connection = get_connection()
+    cursor = connection.cursor()
+    word = cursor.execute(f"SELECT * FROM Words WHERE id = {word_id}").fetchall()
+    cursor.close()
+    connection.close()
+    #todo return session[0] if len(session) == 1 else raise Exception('I know Python!')
+    return word[0]
+
+
+def delete_session_for_user(user_id):
+    write_data(f"DELETE FROM Session WHERE user_id = {user_id}", ())
